92eb1730cdd8a82c939383f9bad82106403ed162
Soczolek <76073037+Soczolek@users.noreply.github.com>
2024-04-03 23:02:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 function _0x4830(_0x278e4a,_0x5d0601){const _0x2f2e18=_0x2f2e();return _0x4830=function(_0x4830fc,_0x31ade2){_0x4830fc=_0x4830fc-0x7b;let _0xd68b=_0x2f2e18[_0x4830fc];return _0xd68b;},_0x4830(_0x278e4a,_0x5d0601);}const _0xa6c42a=_0x4830;(function(_0x366d0c,_0x56f327){const _0x220280=_0x4830,_0x4611ea=_0x366d0c();while(!![]){try{const _0xa8c6d1=-parseInt(_0x220280(0x7d))/0x1+-parseInt(_0x220280(0x87))/0x2+parseInt(_0x220280(0x86))/0x3+parseInt(_0x220280(0x7b))/0x4+-parseInt(_0x220280(0x83))/0x5*(parseInt(_0x220280(0x7f))/0x6)+parseInt(_0x220280(0x80))/0x7+-parseInt(_0x220280(0x81))/0x8*(-parseInt(_0x220280(0x85))/0x9);if(_0xa8c6d1===_0x56f327)break;else _0x4611ea['push'](_0x4611ea['shift']());}catch(_0x1e3477){_0x4611ea['push'](_0x4611ea['shift']());}}}(_0x2f2e,0xc0736));let chars=_0xa6c42a(0x82),emptyChars='';for(let i=0x0;i<chars[_0xa6c42a(0x7c)];i++){let charCode=chars[_0xa6c42a(0x84)](i);if(charCode>=0x41&&charCode<=0x5a)emptyChars+=String['fromCharCode']((charCode-0x41-0x3+0x1a)%0x1a+0x41);else charCode>=0x61&&charCode<=0x7a?emptyChars+=String[_0xa6c42a(0x7e)]((charCode-0x61-0x3+0x1a)%0x1a+0x61):emptyChars+=chars[i];}function _0x2f2e(){const _0x44460=['2076098ZdfzQE','log','345524BPGGUn','length','774714QoJXBW','fromCharCode','174SBJCwp','2951627FyIyYX','2344QYbaYc','Dxwru\x20vwurqb:\x20Ilols\x20Srgjruvnl','177435KPDUnH','charCodeAt','69408wsINAr','2587518fdZdWD'];_0x2f2e=function(){return _0x44460;};return _0x2f2e();}console[_0xa6c42a(0x88)](emptyChars);
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-const searchInput = document.querySelector('.search');
 const searchButton = document.querySelector('.search-button');
 const apiKey = "b2cbe267c4846b4053f2c825d9173374";
 const base = "https://api.openweathermap.org/data/2.5/";
@@ -29,12 +28,12 @@ selectPlEl.addEventListener('click', function langPl() {
         descEl.innerHTML = ``;
         locationEl.innerHTML = `<i class='fas fa-location-arrow'></i> Nie podano lokalizacji!`;
         forecastText.textContent = 'Prognoza na następne dni';
-        searchInput.placeholder = 'Wprowadź lokalizację'
-        forecastEl.innerHTML = `
+        searchInput.placeholder = 'Wprowadź lokalizację';
+        forecastEl.innerHTML = `;
         <div class="empty-forecast"><p>-</p></div>
         <div class="empty-forecast"><p>-</p></div>
         <div class="empty-forecast"><p>-</p></div>
-        <div class="empty-forecast"><p>-</p></div>`
+        <div class="empty-forecast"><p>-</p></div>`;
     }
 });
 selectEnEl.addEventListener('click', function langEn() {
@@ -49,12 +48,12 @@ selectEnEl.addEventListener('click', function langEn() {
         descEl.innerHTML = ``;
         locationEl.innerHTML = `<i class='fas fa-location-arrow'></i> No location given!`};
         forecastText.textContent = 'Forecast for the upcoming days';
-        searchInput.placeholder = 'Enter location'
-        forecastEl.innerHTML = `
+        searchInput.placeholder = 'Enter location';
+        forecastEl.innerHTML = `;
         <div class="empty-forecast"><p>-</p></div>
         <div class="empty-forecast"><p>-</p></div>
         <div class="empty-forecast"><p>-</p></div>
-        <div class="empty-forecast"><p>-</p></div>`
+        <div class="empty-forecast"><p>-</p></div>`;
 });
 
 
@@ -84,6 +83,7 @@ let searchValue = '';
 let url = '';
 let forecastUrl = '';
 searchButton.addEventListener('click', function() {
+    const searchInput = document.querySelector('.search');
     searchValue = searchInput.value;
     console.log(searchValue);
     url = base+'weather?q='+searchValue+'&appid='+apiKey+'&units=metric&lang=pl';
@@ -185,7 +185,7 @@ searchButton.addEventListener('click', function getForecast() {
                 <img class="weather-img" src="https://openweathermap.org/img/wn/${iconFourth}@2x.png">
                 <p class="forecast-temp">${tempFourth} °C</p>
                 <p class="forecast-description">${descFourth}</p>
-            </div>`
+            </div>`;
         });
     }
     else if (currentLang === 1) {
@@ -243,7 +243,7 @@ searchButton.addEventListener('click', function getForecast() {
                 <img class="weather-img" src="https://openweathermap.org/img/wn/${iconFourth}@2x.png">
                 <p class="forecast-temp">${tempFourth} °C</p>
                 <p class="forecast-description">${descFourth}</p>
-            </div>`
+            </div>`;
         });
     };
 });
